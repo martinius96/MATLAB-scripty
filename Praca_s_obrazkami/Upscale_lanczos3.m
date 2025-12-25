6cfeec717bec54ff292e8de6aae1e2d976a36908
Martin Chlebovec <martinius96@gmail.com>
2025-12-25 22:28:55 +0100

    Add Lanczos3 image upscaling script

---
+++ Praca_s_obrazkami/Upscale_lanczos3.m
@@ -0,0 +1,23 @@
+% --- Upscale fotografie 2x algoritmom Lanczos3 (pre ostré hrany) ---
+% Martin Chlebovec
+
+% Vstupny obrazok, nazov vo workspace, priecienku
+input_filename = 'FOTO.jpg';
+[folder, name, ext] = fileparts(input_filename);
+
+% Nacitanie obrazka do premennej
+img = imread(input_filename);
+
+% Kolko krat zväčešujeme
+scaleFactor = 2;
+
+% Realizácia zväčšenia obrázka algoritmom lanczos3
+upscaled_img = imresize(img, scaleFactor, 'lanczos3');
+
+% Nazov suboru s doplnenim upscaledx2 do nazvu
+output_filename = fullfile(folder, [name '_upscaledx2' ext]);
+
+% Ulozenie obrazka do zlozky s .m scriptom
+imwrite(upscaled_img, output_filename);
+
+fprintf('Hotovo! Obrázok bol uložený ako: %s\n', output_filename);
