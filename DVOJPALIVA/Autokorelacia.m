16b940487fd548488f9ca5111b67632365e1f885
Martin Chlebovec <martinius96@gmail.com>
2026-05-31 22:40:20 +0200

    Update Autokorelacia.m

--- DVOJPALIVA/Autokorelacia.m
+++ DVOJPALIVA/Autokorelacia.m
@@ -1,15 +1,15 @@
 clear; clc; close all;
 
-%% 1. DEFINÍCIA SÚBOROV A INTERVALU
-% Upravené na 2 vstupy podľa zadania
+%% Vstupne subory, oznacenie do grafov, farby
 files = {'100_OMV.mp4', '101_TAM.mp4'};
 labels = {'Benzín OMV 100', 'Benzín TAM 101'};
-colors = {[0.85 0.33 0.1], [0 0.45 0.74]}; % Červená pre OMV, Modrá pre TAM
+colors = {[0.85 0.33 0.1], [0 0.45 0.74]}; 
 
+%% TIMESTAMPS vyhodnotenia (pre orezanie 0 a 30 sekunda)
 start_time = 1; 
 end_time = 29;  
 
-% Maximálny časový posun (lag) v sekundách pre autokoreláciu
+%% MAX posun (lag) pre autokorelaciu v sekundach
 max_lag_sec = 0.2; 
 
 figure('Name', 'Autokorelácia signálov motorov', 'Color', 'w', 'Position', [100, 100, 1000, 600]);
@@ -18,8 +18,6 @@ fprintf('--- ANALÝZA PERIODICITY POMOCOU AUTOKORELÁCIE (Interval: %d - %d s) -
 
 for i = 1:length(files)
     try
-        %% 2. NAČÍTANIE A OREZANIE SIGNÁLU
-        % Poznámka: audioread funguje aj pre .mp4 súbory (načíta zvukovú stopu)
         [audio, fs] = audioread(files{i});
         
         if size(audio, 2) > 1
@@ -36,10 +34,9 @@ for i = 1:length(files)
         sig = audio(idx_start:idx_end);
         sig = sig - mean(sig); % Odstránenie DC zložky
         
-        %% 3. VÝPOČET AUTOKORELÁCIE
         max_lag_samples = round(max_lag_sec * fs);
         
-        % Výpočet autokorelácie s normalizáciou na 'coeff'
+        %%Výpočet autokorelácie s normalizáciou na 'coeff'
         [acf, lags] = xcorr(sig, max_lag_samples, 'coeff');
         
         % Prevod lagov zo vzoriek na čas (sekundy)
@@ -55,7 +52,7 @@ for i = 1:length(files)
         xlabel('Časový posun (Lag) [s]', 'FontSize', 10);
         ylabel('Korelačný koeficient [-]', 'FontSize', 10);
         
-        % Nastavenie osí
+        % limit osí
         xlim([-max_lag_sec max_lag_sec]);
         ylim([-0.5 1.1]); 
         
